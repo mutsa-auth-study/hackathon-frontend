55308a39c8c71e5e566f5f2609dced5e481a1fd0
김진호 <rlawlsgh1227@gmail.com>
2023-08-05 14:16:55 +0900

    design: 헤더 디자인 적용
    헤더를 제작했습니다.
    임시로 제작해놓은 것으로 네비게이션 메뉴나 서비스 명, 로고 등이 변동될 예정입니다.
    로그아웃 버튼은 틀만 잡아둔 것으로 작동은 하지 않습니다.

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,91 @@
+import React from "react"
+import { styled } from "styled-components"
+import theme from "../../styles/Theme"
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+
+function Header(props) {
+  return (
+    <HeaderContainer>
+      <Logo>
+        <LogoImage src="" alt="logo" />
+        <ServiceName>한눈에시험</ServiceName>
+      </Logo>
+      <Navigation>
+        <NavigateItem to="/recommend">추천</NavigateItem>
+        <NavigateItem to="/location">고사장 확인</NavigateItem>
+        <NavigateItem to="/mypage">마이페이지</NavigateItem>
+      </Navigation>
+      <Logout>
+        <FontAwesomeIcon icon={faRightFromBracket} />
+        <Text>로그아웃</Text>
+      </Logout>
+    </HeaderContainer>
+  )
+}
+
+export default Header
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: ${theme.componentSize.maxWidth};
+  height: 73px;
+  background-color: ${theme.colors.primaryColor};
+`
+
+const Logo = styled.div`
+  display: flex;
+`
+
+const LogoImage = styled.img``
+
+const ServiceName = styled.div`
+  color: ${theme.colors.white};
+  font-family: "Pretendard";
+  font-weight: 600;
+  font-size: ${theme.fontSizes.logo};
+  text-align: center;
+`
+
+const Navigation = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const NavigateItem = styled(Link)`
+  margin: 0 10px;
+  padding: 0 20px;
+  color: ${theme.colors.white};
+  font-family: "Pretendard";
+  font-weight: 600;
+  font-size: ${theme.fontSizes.navigation};
+  text-decoration: none;
+`
+
+const Logout = styled.div`
+  display: flex;
+  align-items: center;
+  width: 120px;
+  height: 40px;
+  margin-right: 154px;
+  padding: 8px 16px;
+  border-radius: 4px;
+  border: 1px solid ${theme.colors.white};
+
+  color: ${theme.colors.white};
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const Text = styled.div`
+  margin-left: 5px;
+  font-family: "Pretendard";
+  font-size: ${theme.fontSizes.button};
+  font-weight: 600;
+`
---
+++ frontend/src/pages/Location.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { styled } from "styled-components"
+import theme from "../styles/Theme"
+
+function Location(props) {
+  return (
+    <LocationContainer>
+      <></>
+    </LocationContainer>
+  )
+}
+
+export default Location
+
+const LocationContainer = styled.div`
+  width: ${theme.componentSize.maxWidth};
+`
