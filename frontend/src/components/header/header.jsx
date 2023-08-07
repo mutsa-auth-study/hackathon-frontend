6d9d090f2bbd655b8bd5a950d3c8caf3041da729
김진호 <rlawlsgh1227@gmail.com>
2023-08-07 22:30:13 +0900

    design: 헤더 네비게이션 현재 페이지 강조

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 
 function Header(props) {
+  const url = window.location.href.split("/")
+  const page = url[url.length - 1]
   return (
     <HeaderContainer>
       <Logo>
@@ -13,9 +15,15 @@ function Header(props) {
         <ServiceName>한눈에시험</ServiceName>
       </Logo>
       <Navigation>
-        <NavigateItem to="/recommend">추천</NavigateItem>
-        <NavigateItem to="/location">고사장 확인</NavigateItem>
-        <NavigateItem to="/mypage">마이페이지</NavigateItem>
+        <NavigateItem to="/recommend" accent={page === "recommend" ? 1 : 0}>
+          추천
+        </NavigateItem>
+        <NavigateItem to="/location" accent={page === "location" ? 1 : 0}>
+          고사장 확인
+        </NavigateItem>
+        <NavigateItem to="/mypage" accent={page === "mypage" ? 1 : 0}>
+          마이페이지
+        </NavigateItem>
       </Navigation>
       <Logout>
         <FontAwesomeIcon icon={faRightFromBracket} />
@@ -61,7 +69,7 @@ const NavigateItem = styled(Link)`
   padding: 0 20px;
   color: ${theme.colors.white};
   font-family: "Pretendard";
-  font-weight: 600;
+  font-weight: ${props => (props.accent ? 600 : 300)};
   font-size: ${theme.fontSizes.navigation};
   text-decoration: none;
 `
