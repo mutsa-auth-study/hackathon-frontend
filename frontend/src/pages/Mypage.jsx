acaccc9e5641b9a25439a29fe67e168bb236f21f
김진호 <rlawlsgh1227@gmail.com>
2023-08-07 11:23:24 +0900

    feat: 마이페이지 탭 기능 제작
    탭을 클릭하면 해당 탭의 색이 활성화됩니다.

--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,11 +1,20 @@
 import React from "react"
 import { styled } from "styled-components"
 import theme from "../styles/Theme"
+import Tab from "../components/util/tab"
+import Header from "../components/header/header"
+import { useRecoilValue } from "recoil"
+import tab from "../store/atom/tab"
 
 function Mypage(props) {
+  const tabItem = useRecoilValue(tab)
+
   return (
     <MypageContainer>
-      <></>
+      <Header />
+      <Tab />
+      <ContentTitle>{tabItem.title}</ContentTitle>
+      <ContentView>{tabItem.content}</ContentView>
     </MypageContainer>
   )
 }
@@ -14,4 +23,16 @@ export default Mypage
 
 const MypageContainer = styled.div`
   width: ${theme.componentSize.maxWidth};
+`
+
+const ContentTitle = styled.h2`
+  margin-bottom: 80px;
+  font-family: "Pretendard";
+  font-weight: 600;
+  font-size: ${theme.fontSizes.tabtitle};
+  text-align: center;
+`
+
+const ContentView = styled.section`
+  height: 100vh;
 `
