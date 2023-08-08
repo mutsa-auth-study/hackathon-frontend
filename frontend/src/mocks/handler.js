08f4674fca10becaaeb6108711d32c3cc879eda0
김진호 <rlawlsgh1227@gmail.com>
2023-08-08 17:55:48 +0900

    chore: 로그인을 테스트하기 위한 더미데이터 생성

--- frontend/src/mocks/handler.js
+++ frontend/src/mocks/handler.js
@@ -3,6 +3,7 @@ import { favoriteHandler } from "./handlers/favorite"
 import { recentlyHandler } from "./handlers/recently"
 import { mypageHandler } from "./handlers/mypage"
 import { mainHandler } from "./handlers/main"
+import { authHandler } from "./handlers/auth"
 
 // 핸들러를 추가해서 스프레드 연산자로 추가해주면 됩니다.
 export const handlers = [
@@ -11,4 +12,5 @@ export const handlers = [
   ...recentlyHandler,
   ...mypageHandler,
   ...mainHandler,
+  ...authHandler,
 ]
