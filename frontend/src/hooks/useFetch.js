2897d920cc3a036dd61b29affd8856f3dc93ae69
김진호 <rlawlsgh1227@gmail.com>
2023-08-09 15:03:01 +0900

    refactor: useFetch 인자에 body와 headers 추가, 추후에 바디나 accessToken 처리를 위해

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -5,14 +5,14 @@ import { request } from "./../utils/axios"
 // 서버로부터 get으로 정보를 받아올 때 사용
 // parameter: url: base_url 제외한 뒷 부분
 // return: data: 서버에서 받은 값, loading: 로딩 중, error: 에러
-function useFetch(url) {
+function useFetch(url, body = null, headers = null) {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState("")
 
   const fetchData = async () => {
     try {
-      const response = await request("get", url)
+      const response = await request("get", url, body, headers)
       setData(response)
     } catch (error) {
       setError(error)
