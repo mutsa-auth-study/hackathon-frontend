33bec19536675e7bde8917b5a24bf48e4425ffff
김진호 <rlawlsgh1227@gmail.com>
2023-08-09 15:00:38 +0900

    feat: 최근 조회 목록 페이지네이션 구현

--- frontend/src/components/mypage/recentlyInquire.jsx
+++ frontend/src/components/mypage/recentlyInquire.jsx
@@ -2,25 +2,27 @@ import React, { useEffect, useState } from "react"
 import { styled } from "styled-components"
 import useFetch from "../../hooks/useFetch"
 import ExamList from "../eachitem/examList"
+import useCSPagination from "../../hooks/useCSPagination"
 
 function RecentlyInquire(props) {
   const { data, loading, error } = useFetch("/exam/recent")
 
   const [recentlyList, setRecentlyList] = useState([])
+  const { curPageItem, renderCSPagination } = useCSPagination(recentlyList, 1)
 
   useEffect(() => {
     if (data) {
-      console.log(data)
       setRecentlyList(data.information)
     }
   }, [data])
 
   return (
     <RecentlyInquireContainer>
-      {recentlyList &&
-        recentlyList.map(recently => (
+      {curPageItem &&
+        curPageItem.map(recently => (
           <ExamList key={recently.exam_id} eachExam={recently} />
         ))}
+      {renderCSPagination()}
     </RecentlyInquireContainer>
   )
 }
