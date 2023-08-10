faa4b03f175a21ee3e65fa97407d1c6743b715f6
김진호 <rlawlsgh1227@gmail.com>
2023-08-10 23:20:35 +0900

    feat: 메인에도 로딩 중, 에러 결과 표시

--- frontend/src/constants/ErrorMessage.js
+++ frontend/src/constants/ErrorMessage.js
@@ -6,3 +6,6 @@ export const NotExistRecentlyList = `최근 조회한 시험이 없습니다.
 
 export const NotExistReviewList = `작성한 리뷰 항목이 없습니다.
 최소한 1개의 리뷰를 작성한 후, 기능을 이용해 주세요.`
+
+export const ServerError = `일시적인 서버 오류입니다.
+계속 문제가 생긴다면 관리자에게 문의하세요.`
