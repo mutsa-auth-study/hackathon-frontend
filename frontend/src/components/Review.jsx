f60c7a9a0a022e4069cc1b3d9a5790582da6c1e6
jungyungee <86039030+jungyungee@users.noreply.github.com>
2023-08-14 22:06:04 +0900

    feat: 리뷰 스크롤로 보여주는 기능 구현

--- frontend/src/components/Review.jsx
+++ frontend/src/components/Review.jsx
@@ -1,14 +1,53 @@
 import React, { useEffect, useRef, useState } from "react"
 import { styled } from "styled-components"
 import theme from "../styles/Theme"
+import useFetch from "../hooks/useFetch"
 import { useRecoilValue } from "recoil"
 import { currentLocation } from "../store/atom/currentLocation"
+import Header from "./header/header"
+import ReviewList from "./eachitem/reviewList"
+import { user } from "../store/atom/user"
 
 function Review() {
-  const selectedLocation = useRecoilValue(currentLocation)
+  const userinfo = useRecoilValue(user)
+  const { data, loading, error } = useFetch(
+    "/mypage/comment",
+    { user_id: userinfo.user_id },
+    {
+      Authorization: `Bearer ${userinfo.accessToken}`,
+    },
+  )
+
+  const [reviewDataList, setReviewDataList] = useState([]) // 전체 정보
+
+  useEffect(() => {
+    if (data) {
+      setReviewDataList(data.information)
+    }
+  }, [data])
+
   return (
     <ReviewContainer>
+      <Header />
       <Title>고사장 리뷰 보기</Title>
+      <Wrapper>
+        <ScrollableContainer>
+          <StarRate>
+            <AverageRate>평균</AverageRate>
+            <DetailRate>상세 별점</DetailRate>
+          </StarRate>
+          <EachReview>
+            {reviewDataList.length > 0 ? (
+              reviewDataList.map(write => (
+                <ReviewList key={write.location_comment_id} eachWrite={write} />
+              ))
+            ) : (
+              <p>현재 작성된 리뷰가 없습니다</p>
+            )}
+          </EachReview>
+        </ScrollableContainer>
+        <FixedContainer>Fixed Content</FixedContainer>
+      </Wrapper>
     </ReviewContainer>
   )
 }
@@ -16,8 +55,7 @@ function Review() {
 export default Review
 
 const ReviewContainer = styled.div`
-  width: 1287px;
-  margin: 0 auto;
+  width: ${theme.componentSize.maxWidth};
 `
 
 const Title = styled.div`
@@ -26,4 +64,40 @@ const Title = styled.div`
   font-weight: 600;
   font-size: ${theme.fontSizes.subtitle};
   text-align: center;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+`
+
+const ScrollableContainer = styled.div`
+  flex: 1;
+  overflow-y: auto;
+`
+
+const FixedContainer = styled.div`
+  width: 50%;
+  padding: 20px;
+  background-color: #f0f0f0;
+  overflow: hidden;
+`
+
+const StarRate = styled.div`
+  height: 300px;
+  border: solid 1px;
+`
+const AverageRate = styled.div`
+  border: solid 1px;
+  width: 80%;
+  text-align: center;
+`
+const DetailRate = styled.div`
+  border: solid 1px;
+  width: 80%;
+  text-align: center;
+`
+const EachReview = styled.div`
+  border: none;
 `
